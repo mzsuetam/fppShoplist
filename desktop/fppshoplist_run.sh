a43fd2212b8ce2c22327a7119f62fcc2a681afa8
Mateusz Mazur <92544198+mzsuetam@users.noreply.github.com>
2022-05-25 02:55:17 +0200

    Delete desktop directory

--- desktop/fppshoplist_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -f "var/tmp/fpp_shoplist_running" ]
-then
-	touch "var/tmp/fpp_shoplist_running"
-	return=`python3 /home/m_zsuetam/prog/python/fppshoplist/fppshoplist_client.py`
-	rm "var/tmp/fpp_shoplist_running"
-fi
